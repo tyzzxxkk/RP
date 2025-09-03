7fd68748e305e1de67add4f6568804f2030dabab
태연 <s2401@e-mirim.hs.kr>
2025-09-03 23:07:48 +0900

    react

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -11,7 +11,15 @@
 //   return <h1>{name}</h1>;
 // }
 
-export default function App() {
-    return <header><h2>헤더입니다</h2></header>
-  }
+// export default function App() {
+//     return <header><h2>헤더입니다</h2></header>
+//   }
   
+export default function App() {
+    return (
+    <div>
+        <header><h1>헤더</h1></header>
+        <footer><h1>푸터</h1></footer>
+    </div>
+    );
+}
