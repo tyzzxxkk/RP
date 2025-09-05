e4a8455164fac49f5ad0f39fdbe5998ae6f729e5
태연 <s2401@e-mirim.hs.kr>
2025-09-05 23:25:48 +0900

    jsx

---
+++ day5_2.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+export default function App() {
+  const [isOn, setIsOn] = useState(false);
+
+  return (
+    <button onClick={() => setIsOn(!isOn)}>
+      {isOn ? "ON" : "OFF"}
+    </button>
+  );
+}
+
